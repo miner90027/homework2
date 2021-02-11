c58de9508ced2a0ba7392b0f2f483eb1da06b2d2
miner90027 <alekszander.mccormick@outlook.com>
2021-02-11 11:04:43 -0900

    - updated testing code to output labels for the original and the edited string when printing

--- lowerCaseLetters.cpp
+++ lowerCaseLetters.cpp
@@ -5,9 +5,9 @@ void to_lower(char* s);
 
 int main() {
 	char temp[] = "THESE ARE WORDS & OTHER CHARS TO DEMONSTRATE TO_LOWER().";
-	cout << temp << endl;
+	cout << "Original C-style string: " << temp << endl;
 	to_lower(temp);
-	cout << temp << endl;
+	cout << "After running to_lower(): " <<temp << endl;
 	return 0;
 }
 
