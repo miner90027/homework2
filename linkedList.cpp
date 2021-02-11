5147a3464516ba80125955f2e76a7058f9880059
miner90027 <alekszander.mccormick@outlook.com>
2021-02-11 11:44:55 -0900

    - made struct Book() that contains strings for a title, author, and publication year - started working on TEST_CASE to test a list that acts as a queue - initialized 4 different Books & stored them in a list called shelf

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
 #include <list>
-//#include "catch.hpp"
-typedef struct Value{
+#include "catch.hpp"
+using std::string;
+using std::list;
 
-
+struct Book{
+	string title_;
+	string author_;
+	string pubDate_;
 };
+
+TEST_CASE("Test Queue"){
+	Book book1 {"The War of the Worlds", "H.G. Wells", "1898"};
+	Book book2 {"Wine Bottles and Broomsticks","Dave S. Koster","2019"};
+	Book book3 {"A Study in Scarlet", "Sir Arthur Conan Doyle", "1887"};
+	Book book4 {"The Hobbit","J.R.R. Tolkien","1937"};
+
+	list<Book> shelf ={book1, book2, book3, book4};
+}
