6be75113202a0793140fe6611a0ab788194b28e9
miner90027 <alekszander.mccormick@outlook.com>
2021-02-10 20:37:11 -0900

    - implimented code that should convert uppercase letters to lower case letters

--- lowerCaseLetters.cpp
+++ lowerCaseLetters.cpp
@@ -4,21 +4,22 @@ using std::endl;
 void to_lower(char* s);
 
 int main() {
-	cout << "Hello, Program 3!" << endl;
-	to_lower("greetings");
+	char* temp = (char*)"Words on a screen.";
+	cout << temp << endl;
+	to_lower(temp);
+	cout << temp << endl;
 	return 0;
 }
 
 void to_lower(char* s){
+	int caseDiff = 'a' - 'A';
+
 	for(int i =0; s[i] != '\0'; i++)
 	{
-		cout << s[i];
+		if(s[i] >= 'A' && s[i]<='Z')
+		{
+			s[i] = s[i] + caseDiff;
+		}
 	}
 
-	/*
-	int i = 0;
-	while(s[i] != '\0')
-	{
-
-	}*/
 }
