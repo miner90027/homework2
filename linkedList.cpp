c27bc9f8e0e565c0239d4f96c1cab40291da32ce
miner90027 <alekszander.mccormick@outlook.com>
2021-02-11 13:37:28 -0900

    - overloaded the << operator so that it is used to print the book data to a stream - in the queue style test, test to make sure that the first element in the list is the same as book1 by checking the title - started working on a test case to test a stack style list; initialized & filled list called shelf

--- linkedList.cpp
+++ linkedList.cpp
@@ -4,18 +4,48 @@
 #include "catch.hpp"
 using std::string;
 using std::list;
+using std::cout;
+using std::endl;
+using std::ostream;
 
 struct Book{
-	string title_;
-	string author_;
-	string pubDate_;
+	const string title_;
+	const string author_;
+	const int pubDate_;
 };
 
-TEST_CASE("Test Queue"){
-	Book book1 {"The War of the Worlds", "H.G. Wells", "1898"};
-	Book book2 {"Wine Bottles and Broomsticks","Dave S. Koster","2019"};
-	Book book3 {"A Study in Scarlet", "Sir Arthur Conan Doyle", "1887"};
-	Book book4 {"The Hobbit","J.R.R. Tolkien","1937"};
+ostream & operator<< (ostream &os, const Book &book){
+	return os << book.title_ << "\nWritten by: " << book.author_ << "\nPublished in: " << book.pubDate_ << endl;
+}
 
-	list<Book> shelf ={book1, book2, book3, book4};
+//globally declare the four books I will be using in the different tests
+Book book1 {"The War of the Worlds", "H.G. Wells", 1898};
+Book book2 {"Wine Bottles and Broomsticks","Dave S. Koster",2019};
+Book book3 {"A Study in Scarlet", "Sir Arthur Conan Doyle", 1887};
+Book book4 {"The Hobbit","J.R.R. Tolkien",1937};
+
+TEST_CASE("Test Queue style list:"){
+	list<Book> shelf;
+	shelf.push_back(book1);
+	shelf.push_back(book2);
+	shelf.push_back(book3);
+	shelf.push_back(book4);
+
+	cout << shelf.front() << endl;
+	REQUIRE(shelf.front().title_ == book1.title_);
+}
+
+TEST_CASE("Test stack style list"){
+	//make list of books 1-4 in sorted order based on publication date with the newest being the first in the list
+	list<Book> shelf;
+	shelf.push_back(book2);
+	shelf.push_back(book4);
+	shelf.push_back(book1);
+	shelf.push_back(book3);
+
+	//REQUIRE()
+
+	SECTION("Test insert and find"){
+
+	}
 }
