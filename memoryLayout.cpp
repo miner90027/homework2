b10c037d3d99d73912b90c1d60b6c691fcabc088
miner90027 <alekszander.mccormick@outlook.com>
2021-02-12 11:32:44 -0900

    - changed printPointer() to take a reference int rather than make a copy because the memory addresses being printed were the address of the copy not the original - wrote for() loop that itterates over every element in intArray and uses printPointer to print the memory addresses of each element - wrote for() loop that itterates over every space in arrayNew and assigns it to the value of the itterator of the for loop. Then use printPointer to print the memory addresses - initilize a vector of ints - use printPointer to print out memory addressses but it keeps printing the same address for each element in the vector

--- memoryLayout.cpp
+++ memoryLayout.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 #include <vector>
+using std::vector;
 using std::cout;
 using std::endl;
 
-void printPointer(int i);
+void printPointer(int &i);
 
 int main() {
 	int intArray[10] = {0,1,2,3,4,5,6,7,8,9};
 	auto arrayNew = new int[10];
-	/*
+	vector<int> intVec = {0,1,2,3,4,5,6,7,8,9};
+
+
+	cout << "Print contents and memory location of variables allocated to the stack"<< endl;
+	for(auto i: intVec)
+	{
+		printPointer(i);
+	}
+
+	cout << endl << "Print contents and memory location of variables allocated in static data"<< endl;
 	for (int i = 0; i < 10; i++)
 	{
 		printPointer(intArray[i]);
-	}*/
+	}
+
+
+	cout << endl << "Print contents and memory location of variables allocated to the free store"<< endl;
 	for (int i = 0; i < 10; i++)
 	{
-		arrayNew[i]=i;
+		arrayNew[i]= i;
 		printPointer(arrayNew[i]);
 	}
+
 }
-void printPointer(int i) {
+
+void printPointer(int &i) {
 	int* iptr = &i;
 	// the unary * operator dereferences the pointer
 	cout << "i = " << *iptr << endl;
