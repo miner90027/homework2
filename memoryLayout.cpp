ee98a2187b13b4dc91809565e6bcfa3f8ee59465
miner90027 <alekszander.mccormick@outlook.com>
2021-02-12 17:43:25 -0900

    - rearranged the loops and print statements to match the order in which the variables were allocated

--- memoryLayout.cpp
+++ memoryLayout.cpp
@@ -11,25 +11,23 @@ int main() {
 	auto arrayNew = new int[5];
 	vector<int> intVec = {0,1,2,3,4};
 
-
-	cout << "Print contents and memory location of variables allocated to the stack"<< endl;
-	for(auto & i: intVec)
-	{
-		printPointer(i);
-	}
-
-	cout << endl << "Print contents and memory location of variables allocated in static data"<< endl;
+	cout << "Print contents and memory location of variables allocated in static data"<< endl;
 	for (int & i : intArray)
 	{
 		printPointer(i);
 	}
-
 
 	cout << endl << "Print contents and memory location of variables allocated to the free store"<< endl;
 	for (int i = 0; i < 5; i++)
 	{
 		arrayNew[i]= i;
 		printPointer(arrayNew[i]);
+	}
+
+	cout << endl <<  "Print contents and memory location of variables allocated to the stack"<< endl;
+	for(auto & i: intVec)
+	{
+		printPointer(i);
 	}
 
 }
