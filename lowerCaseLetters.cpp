99b7c4c7e4a774a27fa3958c15463c732600b430
miner90027 <alekszander.mccormick@outlook.com>
2021-02-12 20:06:25 -0900

    - changed name of function to_lower() to toLower

--- lowerCaseLetters.cpp
+++ lowerCaseLetters.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-void to_lower(char* s);
+void toLower(char* s);
 
 int main() {
 	char temp[] = "THESE ARE WORDS & OTHER CHARS TO DEMONSTRATE TO_LOWER().";
 	cout << "Original C-style string: " << temp << endl;
-	to_lower(temp);
+	toLower(temp);
 	cout << "After running to_lower(): " <<temp << endl;
 	return 0;
 }
 
-void to_lower(char* s){
+void toLower(char* s){
 	int caseDiff = 'a' - 'A';
 
 	for(int i =0; s[i] != '\0'; i++)
